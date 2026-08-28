47e242a3b63ad95af7e794f5730f310985d7f4b2
AutoID <121750896+AutoID-Alex@users.noreply.github.com>
2026-08-28 08:47:08 +0300

    Bump AutoID Android to v0.3.0

--- android-v0.1/app/build.gradle.kts
+++ android-v0.1/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ro.autoid.app"
         minSdk = 23
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
     }
 
     buildTypes {
